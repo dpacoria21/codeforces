6758d95f65e34f28523be40a848bd77494e0051e
Diego Pacori <dpacoria@unsa.edu.pe>
2023-03-30 09:39:02 -0500

    I resolve Hit the Lottery problem

---
+++ Ejercicios/hitLottery/alien.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <cmath>
+#include <iomanip>
+#include <string>
+
+using ll = long long;
+using ld = long double;
+using namespace std;
+
+int main() {
+    ll n, count = 0;
+    cin >> n;
+    while(n != 0) {
+        if(n>=100) {
+           count++;
+           n -= 100; 
+        }else if(n >= 20) {
+            count++;
+            n -= 20;
+        }else if(n >= 10) {
+            count++;
+            n -= 10;
+        }else if(n >= 5) {
+            count++;
+            n -= 5;
+        }else {
+            count += n;
+            n = 0;
+        }
+    }
+    cout << count;
+    /*
+    ll n;
+    ll arr[5] = {1, 5, 10, 20, 100};
+    cin >> n;
+    int i=4;
+    ll count = 0;
+    while(n != 0) {
+        count += floor(n/arr[i]);
+        n = n-arr[i]*(floor(n/arr[i]));
+        i--;
+    }
+    cout << count; 
+    */
+    
+    return 0;
+}
