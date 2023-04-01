1c6905a9e1aa358f90f6e850efb1661afc4aa93f
Diego Pacori <dpacoria@unsa.edu.pe>
2023-03-31 19:58:34 -0500

    I resolve with map

---
+++ Ejercicios/hq9+/hq.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <cmath>
+#include <iomanip>
+#include <string>
+#include <map>
+
+using ll = long long;
+using ld = long double;
+using namespace std;
+
+int main() {
+    string p;
+    map<char, int>letters;
+    bool flag = false;
+    letters['H'] = 1;
+    letters['Q'] = 2;
+    letters['9'] = 4;
+    cin >> p;
+    for(char c: p) {
+        if(letters[c] != 0) {
+            flag = true;
+            break;
+        }
+    }
+    if(flag) {
+        cout << "YES";
+    }else {
+        cout << "NO";
+    }
+    return 0;
+}
