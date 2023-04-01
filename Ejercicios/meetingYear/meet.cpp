0fa6b990d156e6374eebfe1886f77447aa1458b1
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-01 10:39:38 -0500

    I resolve this problem but now I use one line of code :)

--- Ejercicios/meetingYear/meet.cpp
+++ Ejercicios/meetingYear/meet.cpp
@@ -11,25 +11,26 @@ using namespace std;
 int main() {
     int x1, x2, x3;
     cin >> x1 >> x2 >> x3;
-    int mid = x1;
-    if( (x1 > x2 && x1 < x3) || (x1 > x3 && x1 < x2 ) ) {
-        if(x2 > x3) {
-            cout << x2-x3;
-        }else {
-            cout << x3-x2;
-        }
-    }else if( (x2 > x1 && x2 < x3) || (x2 > x3 && x2 < x1 ) ) {
-        if(x1 > x3) {
-            cout << x1-x3;
-        }else {
-            cout << x3-x1;
-        }
-    }else if ( (x3 > x2 && x3 < x1) || (x3 > x1 && x3 < x2 ) ) {
-        if(x2>x1) {
-            cout << x2-x1;
-        }else {
-            cout << x1-x2;
-        }
-    }
+    cout << max(x1, max(x2, x3)) - min(x1, min(x2, x3));
+    // int mid = x1;
+    // if( (x1 > x2 && x1 < x3) || (x1 > x3 && x1 < x2 ) ) {
+    //     if(x2 > x3) {
+    //         cout << x2-x3;
+    //     }else {
+    //         cout << x3-x2;
+    //     }
+    // }else if( (x2 > x1 && x2 < x3) || (x2 > x3 && x2 < x1 ) ) {
+    //     if(x1 > x3) {
+    //         cout << x1-x3;
+    //     }else {
+    //         cout << x3-x1;
+    //     }
+    // }else if ( (x3 > x2 && x3 < x1) || (x3 > x1 && x3 < x2 ) ) {
+    //     if(x2>x1) {
+    //         cout << x2-x1;
+    //     }else {
+    //         cout << x1-x2;
+    //     }
+    // }
     return 0;
 }
