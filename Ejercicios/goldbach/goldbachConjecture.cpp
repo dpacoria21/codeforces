1d1253e265d9f1b69b30975bb0ffb312962f55cf
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-03 20:51:14 -0500

    I worked with other raz

--- Ejercicios/goldbach/goldbachConjecture.cpp
+++ Ejercicios/goldbach/goldbachConjecture.cpp
@@ -11,29 +11,10 @@ using namespace std;
 int main() {
     ll n;
     cin >> n;
-    int a1 = 4;
-    while(n!=0 && a1 < 12) {
-        if(n-a1 % 2 == 0) {
-            cout << a1 << " " << a1;
-            break;
-        }else {
-            bool flag = false;
-            int count = 0;
-            for(int i=3; i<=(n-a1); i++) {
-                if((n-a1)%i == 0) {
-                    count++;
-                }
-                if(count > 1) {
-                    flag = !flag;
-                    break;
-                }
-            }
-            if(flag) {
-                cout << a1 << " " << n-a1;
-                break;
-            }
-            a1 += 2;
-        }
+    if(n%2 == 0) {
+        cout << 4 << " " << n-4;   
+    }else {
+        cout << 9 << " " << n-9;
     }
     return 0;
 }
