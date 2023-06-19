656ac7858545fc9041219f0d0390d48f229b96f1
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-18 23:19:59 -0500

    I resolved other problems of ps_lab6

--- Ejercicios/ps_lab_6/problem2.cpp
+++ Ejercicios/ps_lab_6/problem2.cpp
@@ -4,34 +4,43 @@ using ll = long long;
 using ld = long double;
 using namespace std;
 
-struct Tdato
-{
-int b;
-char s[100];
+struct Tdato{
+    int b;
+    char s[100];
 };
 
 int main() {
     int x, n, a[10]={1,2,3,4,5,6,7,8,9,0};
     double f;
     char nombre[]="Ejercicios ficheros binarios";
+
     Tdato p;
+    p.b = 125;
+    for(int i=0; i<10; i++) {
+        p.s[i] = nombre[i];
+    }
+
     x = 10;
     f = 10.5;
     ofstream f2;
     ifstream f1;
+
     f2.open("salida.dat", ios::out | ios::binary);
-    f2.write(reinterpret_cast<char *>(&a), sizeof(a));
+    f2.write(reinterpret_cast<char *>(&p), sizeof(Tdato));
     f2.close();
+
     f1.open("salida.dat", ios::in | ios::binary);    
     int y;
-    int arr[10];
+    int arr[5];
+    char letters[10];
     int i = 0;
-    f1.getline(reinterpret_cast<char *>(&arr), sizeof(a));
-    // f1.read(reinterpret_cast<char *>(&y), sizeof(int));
-    // cout << y << "\n";
+    Tdato pRead;
+
+    f1.read(reinterpret_cast<char *>(&pRead), sizeof(Tdato));
+
+    cout << pRead.b << " " << pRead.s << " ";
+    
     f1.close();
-    for(int i=0; i<10; i++) {
-        cout << arr[i] << " ";
-    }
+    
     return 0;
 }
