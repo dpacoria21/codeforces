de13e6c006863aff2f01afc9374057d3842e3ed6
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-21 11:11:59 -0500

    I resolved fashionabLee problem

---
+++ Ejercicios/fashionabLee/solution.cpp
@@ -0,0 +1,24 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+typedef vector<int>vi;
+typedef pair <int, int> pi;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        if(n%4 == 0) {
+            cout << "YES\n";
+        }else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
