ad2bd4c7afb4bb90ec06780f7bdfd4721bb357d7
Diego Pacori <dpacoria@unsa.edu.pe>
2023-11-21 11:38:19 -0500

    I resolved LCMproblem

--- Ejercicios/LCMproblem/solution.cpp
+++ Ejercicios/LCMproblem/solution.cpp
@@ -4,35 +4,63 @@
 
 using namespace std;
 using ll = long long;
-using ld = long double;
-typedef vector<int>vi;
-typedef pair <int, int> pi;
+using db = long double;
+using str = string;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+#define mp make_pair
+#define f first
+#define s second
+
+#define tcT template <class T
+#define tcTU tcT, class U
+tcT > using V = vector<T>;
+tcT, size_t SZ > using AR = array<T, SZ>;
+
+using vi = V<int>;
+using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
+using vpi = V<pi>;
+using vpl = V<pl>;
+using vpd = V<pd>;
+
+// vectores
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+
+// Loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t; cin >> t;
     while(t--) {
-        ll l, r;
+        int l, r;
         cin >> l >> r;
-        ll resp = 1;
-        ll count = 0;
-        for(int i=2; i<=r; i++) {
-            if(count==2) break;
-            while(l%i==0 && r%i==0 && count!=2) {
-                if(resp*i>=l && resp*i<=r) {
-                    resp*=i;
-                    cout << resp << " ";
-                    count++;
-                    l/=i;
-                    r/=i;
-                }else {
-                    break;
-                }
-            }
-
+        if(l*2 > r) {
+            cout << -1 << " " << -1 << "\n";
+        }else {
+            cout << l << " " << l*2 << "\n";
         }
-        cout << "\n";
     }
     return 0;
 }
