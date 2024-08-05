17f7d2b8b0de78c3b311b7640d778a21e1853269
Your Name <dpacoria@unsa.edu.pe>
2024-08-05 00:06:05 -0500

    I add my solution and clear my template

--- excercises/sort_the_array.cpp
+++ excercises/sort_the_array.cpp
@@ -66,7 +66,55 @@ using vpd = V<pd>;
  
 
 void solve(){
+    int n; cin>>n;
+    vl arr(n);
+    for(auto &a: arr) cin>>a;
+    vl aux = arr;
+    sor(aux);
+    map<ll, ll>pos;
+    for(int i = 0; i<n; i++) {
+        pos[i] = aux[i];
+    }
     
+    dbg(arr);
+    int l = 0, r = n-1;
+    for(int i = 0; i<n; i++) {
+        if(arr[i]!=pos[i]) {
+            l = i;
+            break;
+        }else {
+            l = i;
+        }
+    }
+    for(int i = n-1; i>=0; i--) {
+        dbg(arr[i], pos[i]);
+        if(arr[i]!=pos[i]) {
+            r = i;
+            break;
+        }else {
+            r = i;
+        }
+    }
+
+    if(l>r) {
+        cout << "yes\n";
+        cout << "1 1";
+    }else {
+        reverse(arr.begin()+l, arr.begin()+r+1);
+        bool flag = true;
+        for(int i=0 ; i<n; i++) {
+            if(arr[i] != pos[i]) {
+                flag = false;
+                break;
+            }
+        }
+        if(flag) {
+            cout  << "yes\n";
+            cout << l+1 << " " << r+1 << "\n";
+        }else {
+            cout << "no\n";
+        }
+    }
 }
  
 int main() {
